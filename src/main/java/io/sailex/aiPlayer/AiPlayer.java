ddb44dbe6c1e40e8d8fcc844c80f9e4644a60f11
sailex <elias.hahn@mail.de>
2024-10-09 13:24:14 +0200

    Initial commit

---
+++ src/main/java/io/sailex/aiPlayer/AiPlayer.java
@@ -0,0 +1,10 @@
+package io.sailex.aiPlayer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AiPlayer implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/io/sailex/aiPlayer/client/AiPlayerClient.java
@@ -0,0 +1,10 @@
+package io.sailex.aiPlayer.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AiPlayerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
