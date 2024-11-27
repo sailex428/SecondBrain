55348e0b0f0848b6f668510b7f1cdd494a11664c
sailex <elias.hahn@mail.de>
2024-11-27 23:24:01 +0100

    adjust task group

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -6,12 +6,12 @@ stonecutter active "1.20.4" /* [SC] DO NOT EDIT */
 stonecutter.automaticPlatformConstants = true
 
 stonecutter registerChiseled tasks.register("chiseledBuildAll", stonecutter.chiseled) {
-    group = "project"
+    group = "build"
     ofTask("buildBothMods")
 }
 
 tasks.register("buildBothMods") {
-    group = "project"
+    group = "build"
     description = "Builds both subprojects for the current Minecraft version"
     dependsOn(":ai-npc-client:build", ":ai-npc-launcher:build")
 }
