d91ec56f72c826ccb3015f520198d7ddce3aefdd
sailex <elias.hahn@mail.de>
2024-12-21 23:31:33 +0100

    some adjustments logutils

--- ai-npc-launcher/src/main/java/io/sailex/ai/npc/launcher/util/LogUtil.java
+++ ai-npc-launcher/src/main/java/io/sailex/ai/npc/launcher/util/LogUtil.java
@@ -3,6 +3,7 @@ package io.sailex.ai.npc.launcher.util;
 import io.sailex.ai.npc.launcher.AiNPCLauncher;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.MutableText;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import org.apache.logging.log4j.LogManager;
@@ -13,7 +14,7 @@ public class LogUtil {
 	private LogUtil() {}
 
 	private static final Logger LOGGER = LogManager.getLogger(LogUtil.class);
-	private static final MutableText PREFIX =  Text.literal("[").append("AI-NPC").formatted(Formatting.DARK_PURPLE).append("]");
+	private static final MutableText PREFIX = buildPrefix();
 
 	public static void info(String message) {
 		info(message, false);
@@ -22,14 +23,14 @@ public class LogUtil {
 	public static void info(String message, boolean onlyInConsole) {
 		MutableText formattedMessage = formatInfo(message);
 		if (onlyInConsole) {
-			LOGGER.info(formattedMessage.toString());
+			LOGGER.info(formattedMessage.getString());
 		} else {
 			log(formattedMessage);
 		}
 	}
 
 	public static MutableText formatInfo(String message) {
-		return PREFIX.append(message);
+		return Text.literal("").append(PREFIX).append(message);
 	}
 
 	public static void error(String message) {
@@ -39,14 +40,19 @@ public class LogUtil {
 	public static void error(String message, boolean onlyInConsole) {
 		MutableText formattedMessage = formatError(message);
 		if (onlyInConsole) {
-			LOGGER.error(formattedMessage.toString());
+			LOGGER.error(formattedMessage.getString());
 		} else {
 			log(formattedMessage);
 		}
 	}
 
 	public static MutableText formatError(String message) {
-		return PREFIX.append(message).formatted(Formatting.RED);
+		return Text.literal("").append(PREFIX)
+				.append(message).setStyle(Style.EMPTY.withFormatting(Formatting.RED));
+	}
+
+	private static MutableText buildPrefix() {
+		return Text.literal("[AI-NPC] ").formatted(Formatting.DARK_PURPLE);
 	}
 
 	private static void log(MutableText formattedMessage) {
