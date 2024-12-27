ed40252f9a3b7f936a07798404b0512f58574a13
sailex <elias.hahn@mail.de>
2024-12-27 13:05:04 +0100

    add some specific example skills

--- ai-npc-client/src/main/java/io/sailex/ai/npc/client/constant/Instructions.java
+++ ai-npc-client/src/main/java/io/sailex/ai/npc/client/constant/Instructions.java
@@ -27,6 +27,7 @@ public class Instructions {
 				When responding to a request, use action types CHAT, MOVE, MINE, DROP, STOP, CRAFT, depending on the context.
 				Each action should be clearly defined with an appropriate message and the needed fields.
 				Always structure responses like the examples in the "relevant data" section.
+				You could also use the copy the structure and just fill it with your data.
 				You should combine multiple actions (e.g., CHAT, MOVE, DROP) to fulfill the request.
 				e.g. for DROP you must specify just the block name in targetType. For CRAFT you need the whole id in targetType.
 				""";
