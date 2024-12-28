476a72e9dbf9778b7590cf8296dda357e58b0ea8
sailex <elias.hahn@mail.de>
2024-12-28 22:44:57 +0100

    fix: could not open login browser popup window on win11 and linux systems

--- ai-npc-launcher/src/main/java/io/sailex/ai/npc/launcher/constants/ModRepositories.java
+++ ai-npc-launcher/src/main/java/io/sailex/ai/npc/launcher/constants/ModRepositories.java
@@ -8,5 +8,5 @@ public class ModRepositories {
 	private ModRepositories() {}
 
 	public static final VersionSpecificModRepository AI_NPC = new VersionSpecificModRepository(
-			URLs.url("https://github.com/sailex428/AI-NPC/releases/download/"), "ai-npc", "v1.0.8", "-beta");
+			URLs.url("https://github.com/sailex428/AI-NPC/releases/download/"), "ai-npc", "v1.0.9", "-beta");
 }
