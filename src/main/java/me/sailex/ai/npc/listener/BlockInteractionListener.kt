c3a4af2a11c6a3058af04cb0e12df4600a519a1c
sailex <elias.hahn@mail.de>
2025-02-25 18:58:31 +0100

    try to fix some eventListeners, so there're working on server

--- src/main/java/me/sailex/ai/npc/listener/BlockInteractionListener.kt
+++ src/main/java/me/sailex/ai/npc/listener/BlockInteractionListener.kt
@@ -1,6 +1,9 @@
 package me.sailex.ai.npc.listener
 
 import me.sailex.ai.npc.model.NPC
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents
+import net.fabricmc.fabric.api.event.player.UseBlockCallback
+import net.minecraft.util.ActionResult
 
 /**
  * Listens to block interactions of this npc
@@ -9,29 +12,29 @@ class BlockInteractionListener(
     npc: NPC,
 ) : AEventListener(npc) {
     override fun register() {
-//        ClientPlayerBlockBreakEvents.AFTER.register { _, player, pos, state ->
-//            if (player.uuid != npc.entity.uuid) {
-//                return@register
-//            }
-//            val blockBreakMessage =
-//                String.format(
-//                    "You broke the block %s at %s",
-//                    state.block.name.string,
-//                    pos.toShortString(),
-//                )
-//            handleMessage(blockBreakMessage)
-//        }
-//
-//        UseBlockCallback.EVENT.register { player, _, _, hitResult ->
-//            if (player.uuid == npc.entity.uuid) {
-//                val blockInteractionMessage =
-//                    String.format(
-//                        "You used block at %s",
-//                        hitResult.blockPos,
-//                    )
-//                handleMessage(blockInteractionMessage)
-//            }
-//            return@register ActionResult.PASS
-//        }
+        PlayerBlockBreakEvents.AFTER.register { _, player, pos, state, entity ->
+            if (player.uuid != npc.entity.uuid) {
+                return@register
+            }
+            val blockBreakMessage =
+                String.format(
+                    "You broke the block %s at %s",
+                    state.block.name.string,
+                    pos.toShortString(),
+                )
+            handleMessage("system", blockBreakMessage)
+        }
+
+        UseBlockCallback.EVENT.register { player, _, _, hitResult ->
+            if (player.uuid == npc.entity.uuid) {
+                val blockInteractionMessage =
+                    String.format(
+                        "You used block at %s",
+                        hitResult.blockPos,
+                    )
+                handleMessage("system", blockInteractionMessage)
+            }
+            return@register ActionResult.PASS
+        }
     }
 }
--- src/main/java/me/sailex/ai/npc/listener/CombatEventListener.kt
+++ src/main/java/me/sailex/ai/npc/listener/CombatEventListener.kt
@@ -2,6 +2,7 @@ package me.sailex.ai.npc.listener
 
 import me.sailex.ai.npc.callback.PlayerDamageCallback
 import me.sailex.ai.npc.model.NPC
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback
 import net.minecraft.util.ActionResult
 
 class CombatEventListener(
@@ -9,21 +10,21 @@ class CombatEventListener(
 ) : AEventListener(npc) {
 
     override fun register() {
-//        AttackEntityCallback.EVENT.register { player, _, _, entity, hitResult ->
-//            if (player.uuid == npc.entity.uuid) {
-//                var entityAttackMessage = if (hitResult == null) {
-//                    String.format("You tried to attacked entity %s, but you missed your hit", entity.name.string)
-//                } else {
-//                    String.format(
-//                        "You attacked entity %s at %s",
-//                        entity.name.string,
-//                        hitResult.pos,
-//                    )
-//                }
-//                handleMessage(entityAttackMessage)
-//            }
-//            return@register ActionResult.PASS
-//        }
+        AttackEntityCallback.EVENT.register { player, _, _, entity, hitResult ->
+            if (player.uuid == npc.entity.uuid) {
+                var entityAttackMessage = if (hitResult == null) {
+                    String.format("You tried to attacked entity %s, but you missed your hit", entity.name.string)
+                } else {
+                    String.format(
+                        "You attacked entity %s at %s",
+                        entity.name.string,
+                        hitResult.pos,
+                    )
+                }
+                handleMessage("system", entityAttackMessage)
+            }
+            return@register ActionResult.PASS
+        }
 
         PlayerDamageCallback.EVENT.register { attacker, target, damageName, pos ->
             if (attacker != null && target.uuid == npc.entity.uuid) {
