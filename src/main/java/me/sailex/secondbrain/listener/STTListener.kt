0a9cef61b6326bc6357ae5f4cd7e883b12e092b9
sailex <elias.hahn@mail.de>
2025-05-23 20:03:17 +0200

    fix: add npcName to stt error log

--- src/main/java/me/sailex/secondbrain/listener/STTListener.kt
+++ src/main/java/me/sailex/secondbrain/listener/STTListener.kt
@@ -1,9 +1,7 @@
 package me.sailex.secondbrain.listener
 
 import me.sailex.secondbrain.callback.STTCallback
-import me.sailex.secondbrain.event.EventHandler
 import me.sailex.secondbrain.exception.LLMServiceException
-import me.sailex.secondbrain.llm.LLMClient
 import me.sailex.secondbrain.llm.player2.Player2APIClient
 import me.sailex.secondbrain.model.NPC
 import me.sailex.secondbrain.model.stt.STTType
@@ -15,22 +13,22 @@ class STTListener(npcs: Map<UUID, NPC>) : AEventListener(npcs) {
     override fun register() {
         STTCallback.EVENT.register { type ->
             npcs.forEach {
-                performSTTAction(type, it.value.llmClient, it.value.eventHandler)
+                performSTTAction(type, it.value)
             }
         }
     }
 
-    private fun performSTTAction(type: STTType, llmClient: LLMClient, eventHandler: EventHandler) {
+    private fun performSTTAction(type: STTType, npc: NPC) {
         try {
-            if (llmClient is Player2APIClient) {
+            if (npc.llmClient is Player2APIClient) {
                 if (type == STTType.START) {
-                    llmClient.startSpeechToText()
+                    npc.llmClient.startSpeechToText()
                 } else if (type == STTType.STOP) {
-                    eventHandler.onEvent(llmClient.stopSpeechToText())
+                    npc.eventHandler.onEvent(npc.llmClient.stopSpeechToText())
                 }
             }
         } catch (e: LLMServiceException) {
-            LogUtil.errorInChat(e.message)
+            LogUtil.errorInChat("<" + npc.entity.name.string + ">" + e.message)
             LogUtil.error(e)
         }
     }
