6fc7db5cf99c468518308f3e15a3ed6368ac5c15
sailex <elias.hahn@mail.de>
2025-09-26 17:53:37 +0200

    ci: add publish task

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -2,3 +2,12 @@ plugins {
     id("dev.kikugie.stonecutter")
 }
 stonecutter active "1.20.1"
+
+for (version in stonecutter.versions.map { it.version }.distinct())
+
+tasks.register("publish$version") {
+    group = "publishing"
+    dependsOn(stonecutter.tasks.named("publishMods")
+        { metadata.version == version }
+    )
+}
