a85788bf451701e414e9f92240dd99b96294eb49
sailex <hey@sailex.me>
2026-07-21 21:14:28 +0200

    refactor: remove unused code

--- src/main/java/me/sailex/secondbrain/listener/AEventListener.kt
+++ src/main/java/me/sailex/secondbrain/listener/AEventListener.kt
@@ -1,17 +1,8 @@
 package me.sailex.secondbrain.listener
 
 import me.sailex.secondbrain.model.NPC
-import net.minecraft.entity.player.PlayerEntity
 import java.util.UUID
 
 abstract class AEventListener(
     protected val npcs: Map<UUID, NPC>
-) : IEventListener {
-
-    abstract override fun register()
-
-    protected fun getMatchingNpc(player: PlayerEntity): NPC? {
-        return npcs.values.firstOrNull { it.entity.uuid == player.uuid }
-    }
-
-}
+) : IEventListener
